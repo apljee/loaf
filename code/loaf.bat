2092718b0b9247e3958348a11c3a6e2146cf260c
apljee <apljee23@hotmail.com>
2014-04-09 16:53:43 -0400

    created loaf.bat
    i made loaf.bat and a cool acsii title

---
+++ code/loaf.bat
@@ -0,0 +1,8 @@
+@echo off
+echo LOAF
+echo l        eeeeee  ggggggg  eeeeee  nnnnnn       d        ooooooo  fffffff        aaaaaaaa    ppppppp  r        i    l               fffffff   ooooooo   ooooooo  l       sssss
+echo l        e       g        e       n    n       d        o     o  f              a      a    p     p  rrrrrrr       l               f         o     o   o     o  l       s
+echo l        eeee    g        eeee    n    n       d        o     o  fffff          a      a    ppppppp  r        i    l               ffff      o     o   o     o  l       s
+echo l        e       g    gg  e       n    n  dddddd        o     o  f              a      a    p        r        i    l               f         o     o   o     o  l       sssss
+echo l        e       g     g  e       n    n  d    d        o     o  f              a      aa   p        r        i    l               f         o     o   o     o  l           s
+echo lllllll  eeeeee  ggggggg  eeeeee  n    n  dddddd        ooooooo  f              aaaaaaaa a  p        r        i    lllllll         f         ooooooo   ooooooo  llllll  sssss
